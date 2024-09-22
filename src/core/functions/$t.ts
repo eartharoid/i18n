225295066b6dcce0ea414c74eb2185ed4cd61e3d
Isaac <git@eartharoid.me>
2024-09-22 01:20:13 +0100

    feat: use current namespace by default in nested translations

--- src/core/functions/$t.ts
+++ src/core/functions/$t.ts
@@ -9,10 +9,25 @@ export type Parsed = {
 
 export default <Omit<Getter, 'parse'>>{
 	get(locale, original, data: Parsed) {
-		const [, , args, cycle] = original;
+		const [, original_key, args, cycle] = original;
+		const new_ns_index = data.k.indexOf(':');
+		let key = data.k;
+
+		if (new_ns_index === -1) { // no new prefix
+			// add original if exists
+			const original_ns_index = original_key.indexOf(':');
+			if (original_ns_index !== -1) {
+				const original_ns = original_key.slice(0, original_ns_index);
+				key = `${original_ns}:${data.k}`;
+			}
+		} else if (new_ns_index === 0) { // empty prefix
+			// remove first char
+			key = data.k.slice(1);
+		}
+
 		return locale.i18n.t(
 			locale.locale_id,
-			data.k,
+			key,
 			{
 				...args,
 				...Object.fromEntries(Object.entries(data.o || {}).map(([k, v]) => [k, locale.i18n.resolve(args, v)])),
